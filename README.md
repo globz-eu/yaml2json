a61aa9c392adf59150b6a37a812447a2f1177992
Stefan Dieterle <stefand@artcom.de>
2019-09-27 11:44:59 +0200

    Add help messages

--- README.md
+++ README.md
@@ -13,9 +13,10 @@ usage: yaml2json [-h] [--output OUTPUT] input
 Convert YAML file to JSON file
 
 positional arguments:
-  input
+  input                      input file path
 
 optional arguments:
-  -h, --help            show this help message and exit
-  --output OUTPUT, -o OUTPUT
+  -h, --help                 show this help message and exit
+  --output OUTPUT, -o OUTPUT output file path (default: same as input file with
+                             extension replaced with .json)
 ```
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from pathlib import Path, PurePath
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert YAML file to JSON file', prog='yaml2json')
-    parser.add_argument('input', type=str)
-    parser.add_argument('--output', '-o', type=str, default=None)
+    parser.add_argument('input', type=str, help='input file path')
+    parser.add_argument('--output', '-o', type=str, default=None, help='output file path (default: same as input file with extension replaced with .json)')
     args = parser.parse_args()
     input = PurePath(args.input)
     if args.output:
